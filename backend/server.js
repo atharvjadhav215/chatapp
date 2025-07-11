2e15b2c4a2bea7324966dc8aa369d934f69e1679
Atharv Jadhav <jadhavatharv215@gmail.com>
2025-07-11 19:45:55 +0530

    Removed frontend serving logic for separate Vercel deployment

--- backend/server.js
+++ backend/server.js
@@ -38,16 +38,10 @@ app.use("/api/translate", transRoute); // ✅ Translation API route
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/build")));
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
-  );
-} else {
-  app.get("/", (req, res) => {
-    res.send("API is running..");
-  });
-}
+app.get("/", (req, res) => {
+  res.send("API is running..");
+});
+
 // -------------------------- Deployment ------------------------------
 
 // ✅ Create HTTP Server for WebSockets
